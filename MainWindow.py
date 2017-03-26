04a4d25bcaeba49acc466788733942a542e3649f
Jeff Hoogland <jeffhoogland@linux.com>
2017-03-26 01:20:43 -0500

    GUI has a button that starts the server listener now

--- MainWindow.py
+++ MainWindow.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import json
 from os import curdir, sep
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
@@ -8,24 +9,44 @@ from PySide.QtCore import *
 from ui_MainWindow import Ui_MainWindow
 from HexHandler import HexHandler
 
-class MainWindow(QMainWindow, Ui_mainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
         self.assignWidgets()
         self.show()
         
-        self.listener = HTTPServer(('', PORT), HexHandler)
+        self.ourServer = ServerThread()
     
     def assignWidgets(self):
-        self.startServerButton.clicked.connect(self.startPushed)
-        self.stopServerButton.clicked.connect(self.stopPushed)
+        self.serverToggleButton.clicked.connect(self.serverTogglePushed)
     
-    def startPushed(self):
+    def serverTogglePushed(self):
+        if self.ourServer.isRunning():
+            self.serverToggleButton.setEnabled(False)
+            self.ourServer.terminate()
+            while self.ourServer.isRunning():
+                time.sleep(0.01)
+                continue
+            self.serverToggleButton.setText('Start API Listener')
+            self.serverToggleButton.setEnabled(True)
+        else:
+            self.ourServer.start()
+            self.serverToggleButton.setEnabled(False)
+            while not self.ourServer.isRunning():
+                time.sleep(0.01)
+                continue
+            self.serverToggleButton.setText('Stop API Listener')
+            #Stopping the server doesn't currently work nicely. Fix that at some point
+            #self.serverToggleButton.setEnabled(True)
+
+class ServerThread(QThread):
+    def __init__(self, parent = None):
+        QThread.__init__(self, parent)
+        self.listener = HTTPServer(('', 1234), HexHandler)
+    
+    def run(self):
         self.listener.serve_forever()
-    
-    def stopPushed(self):
-        self.listener.socket.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
