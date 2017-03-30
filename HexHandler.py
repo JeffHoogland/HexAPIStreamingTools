84a8f93f792af9ec3de7bb2f9135fe71f8611572
Jeff Hoogland <jeffhoogland@linux.com>
2017-03-30 18:55:05 -0500

    Updated hex cards csv to have AAA cards and PVE cards

--- HexHandler.py
+++ HexHandler.py
@@ -17,7 +17,48 @@ class HexHandler(BaseHTTPRequestHandler):
             self.send_error(404,'File Not Found: %s' % self.path)
 
     def do_GET(self):
-        self.handle_request()
+        #self.handle_request()
+
+        # Handler for the GET requests
+        if self.path == "/":
+                self.path = "/accepts.txt"
+
+        try:
+                # Check the file extension required and
+                # set the right mime type
+
+            sendReply = False
+            if self.path.endswith(".txt"):
+                mimetype = 'text/plain'
+                sendReply = True
+            if self.path.endswith(".html"):
+                mimetype = 'text/html'
+                sendReply = True
+            if self.path.endswith(".jpg"):
+                mimetype = 'image/jpg'
+                sendReply = True
+            if self.path.endswith(".gif"):
+                mimetype = 'image/gif'
+                sendReply = True
+            if self.path.endswith(".js"):
+                mimetype = 'application/javascript'
+                sendReply = True
+            if self.path.endswith(".css"):
+                mimetype = 'text/css'
+                sendReply = True
+
+            if sendReply == True:
+                # Open the static file requested and send it
+                f = open(curdir + sep + self.path)
+                self.send_response(200)
+                self.send_header('Content-type', mimetype)
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+            return
+            
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
 
     def do_POST(self):
         self.handle_request()
